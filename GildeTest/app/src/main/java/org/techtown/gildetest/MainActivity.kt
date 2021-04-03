9b38484ac2102fb155deaea2c8be03863b2299df
Hanyeop <super7615@naver.com>
2021-04-03 23:37:58 +0900

    테스트

--- GildeTest/app/src/main/java/org/techtown/gildetest/MainActivity.kt
+++ GildeTest/app/src/main/java/org/techtown/gildetest/MainActivity.kt
@@ -2,10 +2,17 @@ package org.techtown.gildetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Glide.with(this)
+            .load(R.drawable.dog)
+            .thumbnail(0.2f)
+            .into(imageView)
     }
 }
