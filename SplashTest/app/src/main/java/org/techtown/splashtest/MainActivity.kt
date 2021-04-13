2d68155afede9249fad3566b6cc9d176783a49e9
Hanyeop <super7615@naver.com>
2021-04-13 21:33:50 +0900

    Splash 완성

--- SplashTest/app/src/main/java/org/techtown/splashtest/MainActivity.kt
+++ SplashTest/app/src/main/java/org/techtown/splashtest/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
