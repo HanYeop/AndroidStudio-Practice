50a96c16e40aa55491e7b942dda88938f3790ef5
Hanyeop <super7615@naver.com>
2021-04-15 13:23:24 +0900

    SavedInstanceState Project 완료

--- SavedInstanceState/app/src/main/java/org/techtown/savedinstancestate/MainActivity.kt
+++ SavedInstanceState/app/src/main/java/org/techtown/savedinstancestate/MainActivity.kt
@@ -2,10 +2,24 @@ package org.techtown.savedinstancestate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(savedInstanceState != null){
+            textView.text = savedInstanceState.getString("TEST")
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("TEST","테스트")
+    }
+
+
 }
