0f7a92d42005f7ec292ef65a6a5bc7aab06a4ebf
Hanyeop <super7615@naver.com>
2021-04-20 16:42:16 +0900

    DataBinding Project 완료

--- DataBinding/app/src/main/java/org/techtown/databinding/MainActivity.kt
+++ DataBinding/app/src/main/java/org/techtown/databinding/MainActivity.kt
@@ -2,10 +2,19 @@ package org.techtown.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.techtown.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // lateinit 사용
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // binding class 인스턴스 생성
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.user = User("Yeop","Han")
     }
 }
---
+++ DataBinding/app/src/main/java/org/techtown/databinding/User.kt
@@ -0,0 +1,3 @@
+package org.techtown.databinding
+
+data class User(val firstName: String, val lastName: String)
