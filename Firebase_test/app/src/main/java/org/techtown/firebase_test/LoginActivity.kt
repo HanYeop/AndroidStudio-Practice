cecfaa6d65ef1d058b96c6bef4414c8e44debd2d
Hanyeop <super7615@naver.com>
2021-04-22 01:09:19 +0900

    페이스북 로그인 추가

--- Firebase_test/app/src/main/java/org/techtown/firebase_test/LoginActivity.kt
+++ Firebase_test/app/src/main/java/org/techtown/firebase_test/LoginActivity.kt
@@ -1,28 +1,44 @@
 package org.techtown.firebase_test
 
 import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import android.widget.Toast
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.*
 
 class LoginActivity : AppCompatActivity() {
     private var auth : FirebaseAuth? = null
     private var googleSignInClient : GoogleSignInClient? = null
     private var GOOGLE_LOGIN_CODE = 9001
+    private var callbackManager : CallbackManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         auth = FirebaseAuth.getInstance()
+        callbackManager = CallbackManager.Factory.create()
 
         // 회원가입 창으로
         signupButton.setOnClickListener {
@@ -37,12 +53,42 @@ class LoginActivity : AppCompatActivity() {
         // 구글 로그인 버튼
         googleButton.setOnClickListener { googleLogin() }
 
+        // 페이스북 로그인 버튼
+        facebookButton.setOnClickListener { facebookLogin() }
+
         var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
         googleSignInClient = GoogleSignIn.getClient(this,gso)
     }
+
+//    private fun getHashKey() {
+//        var packageInfo: PackageInfo? = null
+//        try {
+//            packageInfo =
+//                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+//        } catch (e: PackageManager.NameNotFoundException) {
+//            e.printStackTrace()
+//        }
+//        if (packageInfo == null) Log.e("KeyHash", "KeyHash:null")
+//        for (signature in packageInfo!!.signatures) {
+//            try {
+//                val md = MessageDigest.getInstance("SHA")
+//                md.update(signature.toByteArray())
+//                Log.d(
+//                    "KeyHash",
+//                    Base64.encodeToString(md.digest(), Base64.DEFAULT)
+//                )
+//            } catch (e: NoSuchAlgorithmException) {
+//                Log.e(
+//                    "KeyHash",
+//                    "Unable to get MessageDigest. signature=$signature",
+//                    e
+//                )
+//            }
+//        }
+//    }
 
     // 로그아웃하지 않을 시 자동 로그인 , 회원가입시 바로 로그인 됨
     public override fun onStart() {
@@ -93,8 +139,52 @@ class LoginActivity : AppCompatActivity() {
             }
     }
 
+    // 페이스북 로그인 함수
+    fun facebookLogin(){
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, Arrays.asList("public_profile","email"))
+
+        LoginManager.getInstance()
+            .registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult?) {
+                    // 로그인 성공시
+                    handleFacebookAccessToken(result?.accessToken)
+                    // 파이어베이스로 로그인 데이터를 넘겨줌
+                }
+
+                override fun onCancel() {
+
+                }
+
+                override fun onError(error: FacebookException?) {
+
+                }
+            })
+    }
+
+    fun handleFacebookAccessToken(token : AccessToken?){
+        var credential = FacebookAuthProvider.getCredential(token?.token!!)
+
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener{
+                    task ->
+                if(task.isSuccessful){
+                    // 아이디, 비밀번호 맞을 때
+                    moveMainPage(task.result?.user)
+                    Toast.makeText(this,"로그인 성공",Toast.LENGTH_SHORT).show()
+                }else{
+                    // 틀렸을 때
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+
+        callbackManager?.onActivityResult(requestCode,resultCode,data)
+
         if(requestCode == GOOGLE_LOGIN_CODE){
             var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)!!
             // 구글API가 넘겨주는 값 받아옴
