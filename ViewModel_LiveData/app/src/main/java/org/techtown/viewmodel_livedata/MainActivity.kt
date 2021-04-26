ffa09cf31ceae916b744ceacb17b6140bc3aec59
Hanyeop <super7615@naver.com>
2021-04-26 21:15:19 +0900

    DataBinding 추가

--- ViewModel_LiveData/app/src/main/java/org/techtown/viewmodel_livedata/MainActivity.kt
+++ ViewModel_LiveData/app/src/main/java/org/techtown/viewmodel_livedata/MainActivity.kt
@@ -2,44 +2,32 @@ package org.techtown.viewmodel_livedata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import android.util.Log
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import org.techtown.viewmodel_livedata.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     lateinit var mainViewModel : MainViewModel
-    lateinit var button1 : Button
-    lateinit var button2 : Button
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val text = findViewById<TextView>(R.id.numberText)
-
-        button1 = findViewById(R.id.button)
-        button2 = findViewById(R.id.button2)
-        button1.setOnClickListener (this)
-        button2.setOnClickListener (this)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
         // 뷰모델 가져오기
         mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        binding.mainViewModel = mainViewModel
 
         // 관찰하여 데이터 값이 변경되면 호출
         mainViewModel.currentValue.observe(this, Observer {
-            text.text = it.toString()
+            Log.d("check","현재 값 : $it")
+            binding.numberText.text = it.toString()
         })
-
-    }
-
-    override fun onClick(p0: View?) {
-        when(p0){
-            button1 ->
-                mainViewModel.updateValue(actionType.UP)
-            button2 ->
-                mainViewModel.updateValue(actionType.DOWN)
-        }
+        mainViewModel.currentValue2.observe(this, Observer {
+            binding.numberText2.text = it.toString()
+        })
     }
 }
--- ViewModel_LiveData/app/src/main/java/org/techtown/viewmodel_livedata/MainViewModel.kt
+++ ViewModel_LiveData/app/src/main/java/org/techtown/viewmodel_livedata/MainViewModel.kt
@@ -4,31 +4,37 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-enum class actionType{
-    UP, DOWN
-}
+
 // 데이터의 변경사항을 알려주는 라이브 데이터를 가지는 뷰모델
 class MainViewModel : ViewModel() {
 
     // 변경가능한 Mutable 타입의 LiveData
     private val _currentValue = MutableLiveData<Int>()
+    private val _currentValue2 = MutableLiveData<Int>()
 
     // 무결성을 위한 Getter
     val currentValue : LiveData<Int>
         get() = _currentValue
+    val currentValue2 : LiveData<Int>
+        get() = _currentValue2
 
     // 초기값
     init{
         _currentValue.value = 0
+        _currentValue2.value = 0
     }
 
     // Setter
-    fun updateValue(type : actionType){
+    fun updateValue(type : Int){
         when(type){
-            actionType.UP ->
+            1 ->
                 _currentValue.value = _currentValue.value?.plus(1)
-            actionType.DOWN ->
+            2 ->
                 _currentValue.value = _currentValue.value?.minus(1)
         }
+    }
+
+    fun setValue(){
+        _currentValue2.value = _currentValue.value
     }
 }
